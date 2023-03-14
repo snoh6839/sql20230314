06ba9a48ef6927cc15252e85da1b598d786fca3b
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-14 17:28:34 +0900

    insert query

---
+++ insert.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO employees (
+-- 				emp_no
+-- 				,birth_date
+-- 				,first_name
+-- 				,last_name
+-- 				,gender
+-- 				,hire_date
+-- )
+-- VALUES (
+-- 				500000
+-- 				,'1996-12-23'
+-- 				,'Subin'
+-- 				,'Noh'
+-- 				,'F'
+-- 				,'2023-03-14'
+-- );
+
+SELECT *
+FROM employees
+WHERE emp_no = 500000;
